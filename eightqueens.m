a0f761cc86834156bf4e2675a5b81c198842006a
Li Hao <lihaoml@gmail.com>
2015-01-22 00:41:53 +0800

    eightqueens problem - one solution

---
+++ eightqueens.m
@@ -0,0 +1,42 @@
+function eightqueens
+
+  # check if it is safe to place a queen at (row, col) given the previous queens before column (col - 1) are placed in prevQ
+  function safe = isSafe (row, col, prevQ)
+  safe = true;
+  for i = 1:(col-1)
+    if (prevQ(i) == row || prevQ(i) - i == row - col)  # horizontal or diagonal
+      safe = false;
+    endif
+  endfor
+  endfunction
+  
+  # place the queen for the column "col" given the previous placement (before col) in prevQ
+  function succeed = place(prevQ, col)
+  succeed = false;
+  if (col > 8)
+    succeed = true;
+    # display solution
+    solutionMat = zeros(8, 8);
+    for i = 1:8
+      solutionMat(i, prevQ(i)) = 1;
+    endfor
+    solutionMat
+    return
+  endif
+  for r = 1:8  # check each (r, col)
+    if ( isSafe (r, col, prevQ) ) # go to next column
+      prevQ(col) = r;
+      if ( place(prevQ, col+1) )
+        succeed = true;
+        return;
+      endif
+    endif
+  endfor
+  endfunction
+
+  if (place([], 1))
+    "solution found"
+  else
+    "no solution found"
+  end
+endfunction
