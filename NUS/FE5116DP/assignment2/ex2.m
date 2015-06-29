f365150579804bff98b634298080088527f22728
Li Hao <ash.lihao@gmail.com>
2015-06-29 08:31:11 +0800

    assignment 2 soluriton

--- NUS/FE5116DP/assignment2/ex2.m
+++ NUS/FE5116DP/assignment2/ex2.m
@@ -1,4 +1,4 @@
-function bestOf()
+function ex2()
   r = 0.05
   r_a = 0.06
   r_b = 0.03
@@ -8,19 +8,23 @@ function bestOf()
   s_a0 = 100
   s_b0 = 95
   T = 2
-  nPath = 5120
-  BestOfMC(r, r_a, r_b, sigma_a, sigma_b, rho, s_a0, s_b0, T, nPath)
 
-  sigma = sqrt(sigma_a*sigma_a + sigma_b* sigma_b - 2 * rho * sigma_a * sigma_b);
+  % verification for the case of p_a = s_a, p_b = s_b
+
+  p_a = @(sa)(sa - 90);
+  p_b = @(sb)(sb - 88);
+
+  for i = 1:7
+    nPath(i) = 2^(i+13)
+    pv(i) = BestOfMC(r, r_a, r_b, sigma_a, sigma_b, rho, s_a0, s_b0, T, p_a, p_b, nPath(i))
+  endfor
   
-  d1 = (log (s_a0 / s_b0) + r_b - r_a + sigma * sigma * T / 2) / sigma / sqrt(T);
-  d2 = d1 - sigma * sqrt(T);
-
-  analytic = exp(-r_a*T)*s_a0*normcdf(d1) + exp(-r_b*T)*s_b0*(1 - normcdf(d2))
+  plot(nPath, pv);
+  
 endfunction
 
 
-function pv = BestOfMC(r, r_a, r_b, sigma_a, sigma_b, rho, s_a0, s_b0, T, nPath)
+function pv = BestOfMC(r, r_a, r_b, sigma_a, sigma_b, rho, s_a0, s_b0, T, p_a, p_b, nPath)
   corr = [1, rho; rho, 1];
   cholC = chol(corr);
 
@@ -28,12 +32,12 @@ function pv = BestOfMC(r, r_a, r_b, sigma_a, sigma_b, rho, s_a0, s_b0, T, nPath)
     rnds(1, i) = normrnd(0, sqrt(T));
     rnds(2, i) = normrnd(0, sqrt(T));
   endfor
-				% correlate the brownians
+  % correlate the brownians
   ws = cholC' * rnds;
   for i = [1:nPath];
     s_at = s_a0 * exp ((r - r_a - 0.5 * sigma_a*sigma_a)*T + sigma_a * rnds(1, i));
     s_bt = s_b0 * exp ((r - r_b - 0.5 * sigma_b*sigma_b)*T + sigma_b * rnds(2, i));
-    res(i) = max(s_at, s_bt);
+    res(i) = max(p_a(s_at), p_b(s_bt));
   endfor
   pv = exp(-r*T) * mean(res);
 endfunction
